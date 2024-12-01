a3ff4d6727d149271c8c8feeb5a434d33a40f470
RealMangoRage <64402114+RealMangorage@users.noreply.github.com>
2024-11-30 19:18:21 -0800

    Cleanup

--- src/main/java/org/mangorage/cmd/Util.java
+++ src/main/java/org/mangorage/cmd/Util.java
@@ -1,24 +1,9 @@
 package org.mangorage.cmd;
 
-import org.mangorage.cmd.impl.Command;
-import org.mangorage.cmd.impl.CommandDispatcher;
-
 import java.util.Arrays;
 
 public class Util {
     public static String[] shrinkArray(String[] array) {
-        if (array.length <= 1) {
-            return new String[]{};
-        } else {
-            return Arrays.copyOfRange(array, 1, array.length);
-        }
-    }
-
-    public static String removeFirst(String string) {
-        return string.substring(1);
-    }
-
-    public static Command.Builder<CommandDispatcher.GlobalContext> literal() {
-        return Command.literal(CommandDispatcher.GlobalContext.class);
+        return array.length <= 1 ? new String[] {} : Arrays.copyOfRange(array, 1, array.length);
     }
 }
